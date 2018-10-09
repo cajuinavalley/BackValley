be23c947e5f1cd681973aa9584184ca5d99bb88d
Ramon Melo <me@ramonmelo.com>
2018-10-08 23:43:22 -0300

    update admin

--- backvalley/admin.py
+++ backvalley/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Startup, Sector, Role, Employee
 
-admin.site.register(Startup)
-admin.site.register(Sector)
-admin.site.register(Role)
-admin.site.register(Employee)
+
+class StartupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'facebook', 'instagram', 'linkedin')
+
+admin.site.register(Startup, StartupAdmin)
+admin.site.register(Sector, SectorAdmin)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Employee, EmployeeAdmin)
